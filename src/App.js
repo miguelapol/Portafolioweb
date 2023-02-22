5bd35bc994d59e554845b23c62f9d15793b03eeb
miguelapol <apolonio1995@gmail.com>
2023-02-22 14:35:13 -0600

    import components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import './App.css';
+import About from "./components/About";
+import Conctact from "./components/Conctact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
