176f36b7304299b41dfddc23e0ba58565a851c4e
miguelapol <apolonio1995@gmail.com>
2023-02-22 15:45:17 -0600

    configure tailwindCSS

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 
+
 export default function App() {
   return (
-    <main className="text-gray-400-bg-gray-900-body-font">
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar/>
       <About/>
       <Projects/>
