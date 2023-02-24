aefbe28e56c508c2a4f91719e3585115d19e0675
miguelapol <apolonio1995@gmail.com>
2023-02-24 14:00:52 -0600

    contacto

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,63 @@
 export default function Contact(){
+    return (
+<section id="contact" className="relative">
+  <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+    <form
+      netlify
+      name="contact"
+      className="lg:w-full md:w-full flex flex-col md:ml-auto w-2/5 md:py-8 mt-8 md:mt-0">
+      <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+        ¡Contrátame!
+      </h2>
+      <p className="leading-relaxed mb-5">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+        suscipit officia aspernatur veritatis. Asperiores, aliquid?
+      </p>
+      <div className="relative mb-4">
+        <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+          Nombre
+        </label>
+        <br/>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className="md:w-full lg:w-1/2 bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out "
+        />
+      </div>
+      <div className="relative mb-4">
+        <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+          Correo electrónico
+        </label>
+        <br/>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="md:w-full lg:w-1/2 bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+        />
+      </div>
+      <div className="relative mb-4">
+        <label
+          htmlFor="message"
+          className="leading-7 text-sm text-gray-400">
+          Mensaje
+        </label>
+        <br/>
+        <textarea
+          id="message"
+          name="message"
+          className="md:w-full lg:w-1/2 bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+        />
+      </div>
+      <button
+        type="submit"
+        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg md:w-full lg:w-1/2">
+        Enviar
+      </button>
+    </form>
+  </div>
+</section>
 
+      );
 }
