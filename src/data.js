73b315615e18f0db1b99ecd2cc9740aafde963ea
miguelapol <apolonio1995@gmail.com>
2023-03-10 12:12:48 -0600

    edited projects

--- src/data.js
+++ src/data.js
@@ -1,35 +1,35 @@
 export const projects = [
     {
-      title: "React Reserve",
-      subtitle: "MERN Stack",
+      title: "uam citas medicas Back End",
+      subtitle: "Node js",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-1.gif",
-      link: "https://reactbootcamp.com",
+        "Este proyecto consiste en la creación de una aplicación web de citas médicas, que cuenta con un backend que proporciona las funciones necesarias para realizar operaciones CRUD (crear, leer, actualizar y eliminar) en una base de datos. La aplicación también cuenta con una conexión al frontend, lo que permite que los usuarios puedan interactuar con la interfaz gráfica para solicitar citas médicas.",
+      image: "https://user-images.githubusercontent.com/33709574/167510379-e00af8c9-ffb0-4aa7-9ece-83fc39331d6f.gif",
+      link: "https://github.com/miguelapol/api_uam_citas_medicas",
     },
     {
-      title: "React Tracks",
-      subtitle: "React and Python",
+      title: "Pasteleria",
+      subtitle: "Vuex",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-2.gif",
-      link: "https://reedbarger.com",
+        "Este proyecto consiste en la creación de una página web para una pastelería, desarrollada utilizando la biblioteca Vuex de JavaScript. La página simula el proceso de envío de pedidos por parte de los clientes, utilizando la función de almacenamiento local (localstorage) para guardar los datos de los pedidos realizados.",
+      image: "https://github.com/miguelapol/pasteleria_VUEX/raw/main/home_page.png",
+      link: "https://github.com/miguelapol/pasteleria_VUEX",
     },
     {
-      title: "DevChat",
-      subtitle: "React and Firebase",
+      title: "Pokedex API",
+      subtitle: "API",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-3.gif",
-      link: "https://jsbootcamp.com",
+        "Este proyecto consiste en la creación de una página web que consume la API de Pokemon para proporcionar información sobre los diferentes Pokemon. La página ha sido creada utilizando HTML, CSS y JavaScript.",
+      image: "./pokedex.png",
+      link: "https://github.com/miguelapol/pokemon_API",
     },
     {
-      title: "Epic Todo App",
-      subtitle: "React Hooks",
+      title: "Campaña de vacunación",
+      subtitle: "HTML,css y javascript",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-4.gif",
-      link: "https://pythonbootcamp.com",
+        "Este proyecto consiste en la creación de una página web diseñada para promover y educar sobre las campañas de vacunación relacionadas con COVID-19. La página ha sido creada utilizando HTML, CSS y JavaScript.",
+      image: "./vacune.png",
+      link: "https://github.com/miguelapol/vacuna_web",
     },
   ];
   export const testimonials = [
@@ -49,10 +49,10 @@ export const projects = [
     },
   ];
   export const skills = [
+    "HTML",
+    "CSS",
     "JavaScript",
     "React",
-    "Next.js",
     "Node",
-    "GraphQL",
-    "Material UI",
+    "Python",
   ]
